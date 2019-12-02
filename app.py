5207b9e2f261e6d2ec510b29e25cffc64451487a
Freedomisgood <1063052964@qq.com>
2019-12-02 13:49:46 +0800

    基础

---
+++ app.py
@@ -0,0 +1,321 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/1 21:44
+# @Author  : Mrli
+# @FileName: test.py
+# @Cnblogs ：https://nymrli.top/
+import re
+import random
+import requests
+from lxml import etree
+from njupt import Zhengfang
+from bs4 import BeautifulSoup
+from urllib.parse import parse_qs, urlencode
+from config import *
+
+
+# headers = dict([line.split(":",1) for line in raw_headers.split("\n") if line])
+
+
+
+
+# 使用charles抓包
+proxy = {
+    'http': '127.0.0.1:8888'
+}
+
+
+class Course():
+    '''
+    课程测评
+    '''
+    def __init__(self, account, zf):
+        self.zf = zf
+        self.account = account
+
+    @staticmethod
+    def getcourses(account, cookies):
+        '''
+        通过首页,获得所有课程ID
+        :return:
+        '''
+        # 首页
+        index_url = 'http://jwxt.njupt.edu.cn/xs_main.aspx?xh={account}'.format(account=account)
+        html = getattr(requests, 'get')(url=index_url, headers=headers, cookies=cookies)
+        content = etree.HTML(html.text)
+
+        coursesList = []
+        for c in content.xpath('//*[@id="headDiv"]/ul/li[4]/ul/li'):
+            class_url_suffix = c.xpath('a/@href')[0]
+            # 课程编号
+            course_id = parse_qs(class_url_suffix).get('xsjxpj.aspx?xkkh')[0]
+            # url = URL.PREFIX+class_url
+            # class_url = 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh={classID}&xh={stuID}'.\
+            #     format(classID=course_id, stuID=self.account)
+            # coursesUrlList.append(class_url)
+            coursesList.append(course_id)
+        # return coursesList, coursesUrlList
+        return coursesList
+
+    def getFirstVIEWSTATE(self, course_id):
+        '''
+        通过首页的VIEWSTATE,找到指定课程pjkc的__VIEWSTATE
+        :param classID: 爬取到的第一个课程ID
+        :return: 指定课程pjkc的__VIEWSTATE
+        '''
+        data = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            # 需要填充pjkc、__VIEWSTATE、Button1(保存、提交)、DataGrid1:_ctl7:JS1
+            'pjxx': '',
+            'Button1': '保  存',
+        }
+        target_url = 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        view = zf._get_viewstate(target_url)
+        # 获得classID提交需要的首页__VIEWSTATE
+        data['__VIEWSTATE'] = view
+        # 指定需要获取课程的__VIEWSTATE
+        data['pjkc'] = course_id
+        r = zf.post(target_url, data=data)
+        # print(r.text)
+        soup = BeautifulSoup(r.content, 'lxml')
+        viewstate = soup.find('input', attrs={"name": "__VIEWSTATE"}).get("value")
+        # print(viewstate)
+        return viewstate
+
+    def saveComment(self, course_id, data):
+        '''
+        编码数据,并保存
+        :param classID: 需要评价的课程
+        :param data: 未编码的数据
+        :return:
+        '''
+        target_url = 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        data['Button1'] = '保  存'
+        data_gb2312 = urlencode(data, encoding='gb2312')
+        html = zf.post(target_url, data=data_gb2312, proxies=proxy, headers=headers)
+        if html.status_code == 200:
+            print("成功提交")
+
+    def commitComment(self, course_id, data):
+        '''
+        编码数据,并提交
+
+        :param classID: 需要评价的课程
+        :param data: 未编码的数据
+        :return:
+        '''
+        target_url = 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        data['Button2'] = '提  交'
+        data_gb2312 = urlencode(data, encoding='gb2312')
+        html = zf.post(target_url, data=data_gb2312, proxies=proxy, headers=headers)
+        if html.status_code == 200:
+            print("成功提交")
+
+    def getInputnums(self, course_id):
+        '''
+        通过课程ID获得下面需要提交的选项, 返回一个dict
+        :param course_id:
+        :return:
+        '''
+        course_url = 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh={classID}&xh={stuID}'. \
+            format(classID=course_id, stuID=self.account)
+        # 获取网页
+        html = zf.get_soup(course_url)
+        option = html.find_all('select', attrs={'id': re.compile('DataGrid1__ctl\d+_JS\d+')})
+        # print(option)
+        if option:
+            # 获得最后一个选项的id内容
+            title = option[-1].get('id')
+            # 处理选项信息
+            lastinput = re.search('DataGrid1__ctl(\d+)_JS(\d+)', title)
+            row, col = int(lastinput.group(1)), int(lastinput.group(2))
+            data = {}
+            if row and col:
+                for c in range(1, col+1): # 从1开始
+                    for r in range(2, row+1): # 从2开始
+                        # data['DataGrid1:_ctl{rank}:JS1'.format(rank=rank)] = map_dict.get('完全认同')
+                        data['DataGrid1:_ctl{row}:JS{col}'.format(row=r, col=c)] = '完全认同'
+                    # data['DataGrid1:_ctl{rank}:JS1'.format(rank=random.randint(2, biggest))] = map_dict.get('勉强认同')
+                    data['DataGrid1:_ctl{row}:JS{col}'.format(row=random.randint(2, row), col=c)] = '勉强认同'
+            else:
+                raise Exception('选项获取失败')
+            return data
+
+    def run(self, courses: list):
+        for n, c in enumerate(courses):
+            base = {
+                '__EVENTTARGET': '',
+                '__EVENTARGUMENT': '',
+                # 需要填充pjkc、__VIEWSTATE、Button1(保存、提交)、DataGrid1:_ctl7:JS1
+                'pjxx': '',
+                # 'Button1': '保  存',
+            }
+            # 生成选项
+            grids_data = self.getInputnums(c)
+            # print(grids_data)
+            # 找到该课程的__VIEWSTATE
+            base['__VIEWSTATE'] = self.getFirstVIEWSTATE(c)
+            # 指定课程
+            base['pjkc'] = c
+            # 提交选项-->使用不同函数决定保存还是提交
+            # base['Button1'] = '保  存'
+            # 合并数据
+            postdata = dict(base, **grids_data)
+            self.saveComment(c, postdata)
+            if n == len(courses)-1:
+                self.commitComment(c, postdata)
+
+
+class Teacher():
+    '''
+    教师测评
+    '''
+    def __init__(self, account, zf):
+        self.zf = zf
+        self.account = account
+
+
+    def getFirstVIEWSTATE(self, course_id):
+        '''
+        通过首页的VIEWSTATE,找到指定课程pjkc的__VIEWSTATE
+        :param classID: 爬取到的第一个课程ID
+        :return: 指定课程pjkc的__VIEWSTATE
+        '''
+        data = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            # 需要填充pjkc、__VIEWSTATE、Button1(保存、提交)、DataGrid1:_ctl7:JS1
+            'pjxx': '',
+            'Button1': '保  存',
+        }
+        target_url = 'http://jwxt.njupt.edu.cn/xsjxpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        view = zf._get_viewstate(target_url)
+        # 获得classID提交需要的首页__VIEWSTATE
+        data['__VIEWSTATE'] = view
+        # 指定需要获取课程的__VIEWSTATE
+        data['pjkc'] = course_id
+        r = zf.post(target_url, data=data)
+        # print(r.text)
+        soup = BeautifulSoup(r.content, 'lxml')
+        viewstate = soup.find('input', attrs={"name": "__VIEWSTATE"}).get("value")
+        return viewstate
+
+    def getInputnums(self, course_id):
+        '''
+        通过课程ID获得下面需要提交的选项, 返回一个dict
+        :param course_id:
+        :return:
+        '''
+        course_url = 'http://jwxt.njupt.edu.cn/xsjxpj.aspx?xkkh={classID}&xh={stuID}'. \
+            format(classID=course_id, stuID=self.account)
+        # 获取网页
+        html = zf.get_soup(course_url)
+        # 找到最后一个选项
+        option = html.find_all('select', attrs={'id': re.compile('DataGrid1__ctl\d+_JS\d+')})
+        if option:
+            # 获得最后一个选项的id内容
+            title = option[-1].get('id')
+            # 处理选项信息
+            lastinput = re.search('DataGrid1__ctl(\d+)_JS(\d+)', title)
+            row, col = int(lastinput.group(1)), int(lastinput.group(2))
+            data = {}
+            if row and col:
+                for c in range(1, col+1): # 从1开始
+                    for r in range(2, row+1): # 从2开始
+                        data['DataGrid1:_ctl{row}:JS{col}'.format(row=r, col=c)] = '好'
+                        data['DataGrid1:_ctl{row}:txtjs{col}'.format(row=r, col=c)] = ''
+                    randOption = random.randint(2, row)
+                    data['DataGrid1:_ctl{row}:JS{col}'.format(row=randOption, col=c)] = '较好'
+                    data['DataGrid1:_ctl{row}:txtjs{col}'.format(row=randOption, col=c)] = ''
+            else:
+                raise Exception('选项获取失败')
+            return data
+
+
+    def saveComment(self, course_id, data):
+        '''
+        编码数据,并保存
+        :param classID: 需要评价的课程
+        :param data: 未编码的数据
+        :return:
+        '''
+        target_url = 'http://jwxt.njupt.edu.cn/xsjxpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        data['Button1'] = '保  存'
+        data['txt1'] = ''
+        data['TextBox1'] = '0'
+        data_gb2312 = urlencode(data, encoding='gb2312')
+        html = zf.post(target_url, data=data_gb2312, proxies=proxy, headers=headers)
+        if html.status_code == 200:
+            print("成功提交")
+
+    def commitComment(self, course_id, data):
+        '''
+        编码数据,并提交
+        :param classID: 需要评价的课程
+        :param data: 未编码的数据
+        :return:
+        '''
+        target_url = 'http://jwxt.njupt.edu.cn/xsjxpj.aspx?xkkh={classID}&xh={account}'. \
+            format(classID=course_id, account=self.account)
+        data['Button2'] = '提  交'
+        data['txt1'] = ''
+        data['TextBox1'] = '0'
+        data_gb2312 = urlencode(data, encoding='gb2312')
+        html = zf.post(target_url, data=data_gb2312, proxies=proxy, headers=headers)
+        if html.status_code == 200:
+            print("成功提交")
+
+    def run(self, courses: list):
+        for n, c in enumerate(courses):
+            base = {
+                '__EVENTTARGET': '',
+                '__EVENTARGUMENT': '',
+                # 需要填充pjkc、__VIEWSTATE、Button1(保存、提交)、DataGrid1:_ctl7:JS1
+                'pjxx': '',
+                # 'Button1': '保  存',
+            }
+            # 生成选项
+            grids_data = self.getInputnums(c)
+            # 找到该课程的__VIEWSTATE
+            base['__VIEWSTATE'] = self.getFirstVIEWSTATE(c)
+            # # 指定课程
+            base['pjkc'] = c
+            # # 合并数据
+            postdata = dict(base, **grids_data)
+            self.saveComment(c, postdata)
+            if n == len(courses)-1:
+                self.commitComment(c, postdata)
+
+
+class AutoJudge(Zhengfang):
+    def __init__(self, *args, **kwargs):
+        super(AutoJudge, self).__init__(*args, **kwargs)
+        self.account = kwargs.get('account')
+        self.c = Course(account=self.account, zf=self)
+        self.t = Course(account=self.account, zf=self)
+
+    def run(self):
+        # 获得该学期所有课程
+        courses = Course.getcourses(account=self.account, cookies=self.cookies)
+        print(courses)
+        # 完成课程评价
+        self.c.run(courses)
+        print('---课程评价完成---')
+        # # 完成老师评价
+        # self.t.run(courses)
+        # print('---教师评价完成---')
+
+
+if __name__ == '__main__':
+    # account = 'B17050323'
+    # password = 'sxh7577992211'
+    # zf = Zhengfang(account, password)
+    agent = AutoJudge(account='B17050322', password='aaa123123')
+    agent.run()
+
+
---
+++ config.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/2 11:21
+# @Author  : Mrli
+# @FileName: config.py.py
+# @Cnblogs ：https://nymrli.top/
+
+headers= {
+'Host': 'jwxt.njupt.edu.cn',
+ 'Pragma': 'no-cache',
+ 'Cache-Control': 'no-cache',
+ 'Origin': 'http://jwxt.njupt.edu.cn',
+ 'Upgrade-Insecure-Requests': '1',
+ 'Content-Type': 'application/x-www-form-urlencoded',
+ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+ 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3', 'Referer': 'http://jwxt.njupt.edu.cn/xs_jsmydpj.aspx?xkkh=(2019-2020-1)-B0502032S-ZDHY0001-1&xh=B17050322', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language':
+ 'zh-CN,zh;q=0.9,en;q=0.8',
+ 'Cookie': '_ga=GA1.3.1265485040.1573983252; ASP.NET_SessionId=1pyeqm55tndohvqz3ceq2tvi; WEB=20111157', 'Connection': 'keep-alive'
+}
+# data = {
+#   '__EVENTTARGET': '',
+#   '__EVENTARGUMENT': '',
+#   'DataGrid1:_ctl2:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl3:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl4:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl5:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl6:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl7:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl8:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl9:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl10:JS1': quote('完全认同', encoding='gb2312'),
+#   'DataGrid1:_ctl11:JS1': quote('完全认同', encoding='gb2312'),
+#   'pjxx': '',
+#    'Button1': '保  存',
+# }
+# 不编码
+data = {
+  '__EVENTTARGET': '',
+  '__EVENTARGUMENT': '',
+  'DataGrid1:_ctl2:JS1': '完全认同',
+  'DataGrid1:_ctl3:JS1': '完全认同',
+  'DataGrid1:_ctl4:JS1': '完全认同',
+  'DataGrid1:_ctl5:JS1': '完全认同',
+  'DataGrid1:_ctl6:JS1': '完全认同',
+  'DataGrid1:_ctl7:JS1': '完全认同',
+  'DataGrid1:_ctl8:JS1': '完全认同',
+  'DataGrid1:_ctl9:JS1': '完全认同',
+  'DataGrid1:_ctl10:JS1': '完全认同',
+  'DataGrid1:_ctl11:JS1': '完全认同',
+  'pjxx': '',
+   'Button1': '保  存',
+}
+
+# 编码
+# data = {
+#         '__EVENTTARGET': '',
+#         '__EVENTARGUMENT': '',
+#         # 电机
+#         'DataGrid1:_ctl2:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl3:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl4:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl5:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl6:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl7:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl8:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl9:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl10:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'DataGrid1:_ctl11:JS1': '%CD%EA%C8%AB%C8%CF%CD%AC',
+#         'pjxx': '',
+#         'Button1': '%B1%A3++%B4%E6',
+#     }
